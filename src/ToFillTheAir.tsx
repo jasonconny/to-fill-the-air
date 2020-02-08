e5511fcd47f43e4d199e2daf53111f202b8735dc
Jason Conny <jasonconny@gmail.com>
2020-02-08 08:54:46 -0600

    add Helmet and ScrollToTop

--- src/ToFillTheAir.tsx
+++ src/ToFillTheAir.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import ErrorBoundary from './components/ErrorBoundary';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './routing/ScrollToTop';
 import Routes from './routing/Routes';
 
 const ToFillTheAir: React.FC = () => {
 	return (
 		<ErrorBoundary>
+			<Helmet>
+				<title>
+					To Fill The Air | Grateful Dead set lists
+				</title>
+			</Helmet>
+
 			<BrowserRouter forceRefresh={false}>
+				<ScrollToTop/>
+
 				<Routes/>
 			</BrowserRouter>
 		</ErrorBoundary>
