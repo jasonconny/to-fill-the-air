f7a6b39244c33ebc176a8370bc499f136cc07af0
Jason Conny <jasonconny@gmail.com>
2021-05-28 10:15:46 -0500

    add currentYear to Copyright

--- client/src/components/Layouts/Copyright/Copyright.tsx
+++ client/src/components/Layouts/Copyright/Copyright.tsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import styles from './Copyright.scss';
 
 export const Copyright: React.FC = () => {
+    const [currentYear, setCurrentYear] = React.useState<number | null>(null);
+
+    React.useEffect(() => {
+        setCurrentYear(new Date().getFullYear());
+    }, []);
+
     return (
         <div className={styles.block}>
-            <p>&copy; 2020 To Fill The Air, LLC. All rights reserved.</p>
+            <p>&copy; 2020 - {currentYear} To Fill The Air, LLC. All rights reserved.</p>
         </div>
     );
 };
