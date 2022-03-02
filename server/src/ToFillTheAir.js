a7dbf94d96cc370b71c4811a518e96d00e93165e
Jason Conny <jasonconny@gmail.com>
2022-03-02 14:49:41 -0600

    add basic queries for shows and venues

--- server/src/ToFillTheAir.js
+++ server/src/ToFillTheAir.js
@@ -3,9 +3,37 @@ import { SQLDataSource } from 'datasource-sql';
 const MINUTE = 60;
 
 export default class ToFillTheAir extends SQLDataSource {
+    getAllShows() {
+        return this.knex
+            .select('*')
+            .from('shows')
+            .orderBy('date')
+            .cache(MINUTE);
+    }
+
     getAllSongRefs() {
         return this.knex
             .select('*')
-            .from('song_refs');
+            .from('song_refs')
+            .orderBy('name')
+            .cache(MINUTE);
+    }
+
+    getAllVenues() {
+        return this.knex
+            .select('*')
+            .from('venues')
+            .orderBy('name')
+            .cache(MINUTE);
+    }
+
+    getVenueByName(venueName) {
+        return this.knex
+            .select('*')
+            .from('venues')
+            .where({
+                name: venueName
+            })
+            .cache(MINUTE)
     }
 }
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -10,10 +10,10 @@ export const resolvers = {
         shows: (parent, args, context, info) => parent.getShows(),
     },
     Query: {
-        shows: async () => db.shows.findAll(),
+        shows: async (_, __, { dataSources }) => dataSources.toFillTheAir.getAllShows(),
         show: async (parent, args, {db}, info) => db.shows.findByPk(args.id),
         song_refs: async (_, __, { dataSources }) => dataSources.toFillTheAir.getAllSongRefs(),
-        venues: async () => db.venues.findAll(),
+        venues: async (_, __, { dataSources }) => dataSources.toFillTheAir.getAllVenues(),
         venue: (parent, args, {db}, info) => db.venues.findByPk(args.id)
     }
 };
