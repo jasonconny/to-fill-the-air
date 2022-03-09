a036162e9200d305d2eadc47c886e6a3b9e5c8bc
Jason Conny <jasonconny@gmail.com>
2022-03-08 21:37:15 -0600

    apollo client config

--- client/src/ToFillTheAir.tsx
+++ client/src/ToFillTheAir.tsx
@@ -1,26 +1,38 @@
+import {ApolloClient, ApolloProvider, NormalizedCacheObject} from '@apollo/client';
+import { apolloCache } from './apolloCache';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import env from 'react-dotenv';
 import { Helmet } from 'react-helmet';
 import ErrorBoundary from './components/ErrorBoundary';
 import GlobalStyles from './components/GlobalStyles/GlobalStyles';
 import AppProvider from './providers/AppProvider';
 import Router from './router';
 
+const { APOLLO_SERVER_URI } = env;
+
+const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache: apolloCache,
+    uri: APOLLO_SERVER_URI
+});
+
 const ToFillTheAir: React.FC = () => {
     return (
-        <ErrorBoundary>
-            <Helmet>
-                <title>
-					To Fill The Air | Grateful Dead set lists
-                </title>
-            </Helmet>
+        <ApolloProvider client={apolloClient}>
+            <ErrorBoundary>
+                <Helmet>
+                    <title>
+                        To Fill The Air | Grateful Dead set lists
+                    </title>
+                </Helmet>
 
-            <GlobalStyles/>
+                <GlobalStyles/>
 
-            <AppProvider>
-                <Router/>
-            </AppProvider>
-        </ErrorBoundary>
+                <AppProvider>
+                    <Router/>
+                </AppProvider>
+            </ErrorBoundary>
+        </ApolloProvider>
     );
 };
 
