9fb3c4fbd731f49f6e193d8ed98b53729197114e
Jason Conny <jasonconny@gmail.com>
2022-03-08 21:51:22 -0600

    get song_refs

--- client/src/views/SongsView/SongsView.tsx
+++ client/src/views/SongsView/SongsView.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+export const GET_SONG_REFS = gql`
+    query GetSongRefs {
+        songRefs {
+            song_ref_id
+            title
+            composer
+        }
+    }
+`;
 
 const SongsView: React.FC = () => {
+    const { data } = useQuery(GET_SONG_REFS);
     return (
         <section>
             <h2>Songs</h2>
+
+            {data && data.songRefs ? (
+                <ul>
+                    {data.songRefs.map((songRef: any) => (
+                        <li key={songRef.song_ref_id}>
+                            {songRef.title}<br/>
+                            {songRef.composer}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>no songs</p>
+            )}
         </section>
     );
 };
