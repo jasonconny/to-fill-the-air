a00cd67e674693a688e93564ef613341185b00ce
Jason Conny <jasonconny@gmail.com>
2023-08-21 22:00:18 -0500

    add eslint-config-prettier; update eslint error/warn format

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,63 +1,64 @@
 const path = require('path');
 module.exports = {
-	"env": {
-		"browser": true,
-		"node": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react/recommended"
-	],
-    "parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"project": path.resolve(__dirname, "./tsconfig.json"),
-		"sourceType": "module",
-		"tsconfigRootDir": __dirname
-	},
-    "plugins": [
-        "@typescript-eslint",
-        "react",
-        "react-hooks"
-    ],
-	"root": true,
-    "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/indent": "error",
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/type-annotation-spacing": "error",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-use-before-define": "error",
-        "curly": "error",
-        "default-case": "error",
-        "dot-notation": "error",
-        "eol-last": "off",
-        "guard-for-in": "error",
-        "no-bitwise": "error",
-        "no-caller": "error",
-        "no-console": "off",
-        "no-debugger": "error",
-        "no-empty": "error",
-        "no-empty-function": "error",
-        "no-eval": "error",
-        "no-fallthrough": "error",
-        "no-multiple-empty-lines": "error",
-        "no-new-wrappers": "error",
-        "no-unused-labels": "error",
-        "quotes": [2, "single"],
-        "radix": "error",
-        "react/no-unescaped-entities": "off",
-        "react/prop-types": "off",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "sort-keys": "warn",
-    },
-	"settings": {
-		"react": {
-			"version": "detect"
-		}
-	}
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "project": path.resolve(__dirname, "./tsconfig.json"),
+    "sourceType": "module",
+    "tsconfigRootDir": __dirname
+  },
+  "plugins": [
+      "@typescript-eslint",
+      "react",
+      "react-hooks"
+  ],
+  "root": true,
+  "rules": {
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/interface-name-prefix": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/type-annotation-spacing": 2,
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-use-before-define": 2,
+    "curly": 2,
+    "default-case": 2,
+    "dot-notation": 2,
+    "eol-last": 0,
+    "guard-for-in": 2,
+    "no-bitwise": 2,
+    "no-caller": 2,
+    "no-console": 0,
+    "no-debugger": 2,
+    "no-empty": 2,
+    "no-empty-function": 2,
+    "no-eval": 2,
+    "no-fallthrough": 2,
+    "no-multiple-empty-lines": 2,
+    "no-new-wrappers": 2,
+    "no-unused-labels": 2,
+    "quotes": [2, "single"],
+    "radix": 2,
+    "react/no-unescaped-entities": 0,
+    "react/prop-types": 0,
+    "react-hooks/rules-of-hooks": 2,
+    "react-hooks/exhaustive-deps": 1,
+    "sort-keys": 1,
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
 };
