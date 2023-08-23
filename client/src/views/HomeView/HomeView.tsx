fe989d50df25fc5485d5b799449101d7902ea11b
Jason Conny <jasonconny@gmail.com>
2023-08-22 21:57:13 -0500

    add greeting to home

--- client/src/views/HomeView/HomeView.tsx
+++ client/src/views/HomeView/HomeView.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const HomeView: React.FC = () => {
+  const { isAuthenticated, user } = useAuth0();
+
   return (
     <section>
       <p>Grateful Dead set lists.</p>
+
+      {isAuthenticated ? <p>Welcome {user?.name}</p> : null}
     </section>
   );
 };
